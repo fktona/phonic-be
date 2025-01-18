3b1289f8edfc09f2987b109fda60b73a7d8bdb6b
fktona <adetona.fk@gmail.com>
2025-01-18 03:54:42 +0100

    Feat/ Playgrounf\d setup

--- src/services/market-cap.service.ts
+++ src/services/market-cap.service.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import prisma from '../client';
-import exp from 'constants';
 
 const fetchMarketCap = async (tokenAddress: string): Promise<number | null> => {
   try {
